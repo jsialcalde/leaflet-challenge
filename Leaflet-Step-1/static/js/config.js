d0141a3b00e0e5049087dae902c424a888645935
jsialcalde <j.si.alcalde@gmail.com>
2019-09-28 10:12:36 -0400

    draft

---
+++ Leaflet-Step-1/static/js/config.js
@@ -0,0 +1,4 @@
+// API key
+const API_KEY = "pk.eyJ1IjoianNpYWxjYWxkZSIsImEiOiJjazA5bWhibWwwM3F6M21veDVzYXBya3g0In0.-JuuK1F2QfVHLGJav-Z12g";
+
+// API_KEY = "pk.eyJ1IjoianNpYWxjYWxkZSIsImEiOiJjazA5bWRweGQwNDQ3M2htaHJlYWs3Z3VpIn0.Nn7-xX4xATCMQTsAaXo3hw";
